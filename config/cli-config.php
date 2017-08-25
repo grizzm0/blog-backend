a3838b46b24699331d733a87958059303ba9b5c9
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2017-08-25 06:31:00 +0200

    Initial support for migrations

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,10 +1,21 @@
 <?php
 
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Doctrine\ORM\EntityManager;
-
 require __DIR__ . '/bootstrap.php';
 
-return ConsoleRunner::createHelperSet(
-    (require __DIR__ . '/container.php')->get(EntityManager::class)
-);
+$container     = require __DIR__ . '/container.php';
+$entityManager = $container->get(\Doctrine\ORM\EntityManager::class);
+
+$helperSet = new \Symfony\Component\Console\Helper\HelperSet([
+    'db'     => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($entityManager->getConnection()),
+    'em'     => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($entityManager),
+    'dialog' => new \Symfony\Component\Console\Helper\QuestionHelper(),
+]);
+
+return \Doctrine\ORM\Tools\Console\ConsoleRunner::createApplication($helperSet,[
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand(),
+    new \Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand(),
+])->run();
---
+++ migrations/Version20170825034714.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * @package Migrations
+ */
+class Version20170825034714 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'postgresql',
+            'Migration can only be executed safely on \'postgresql\'.'
+        );
+
+        $this->addSql('CREATE TABLE blog_post (
+          id UUID NOT NULL, 
+          title VARCHAR(128) NOT NULL, 
+          content TEXT NOT NULL, 
+          created TIMESTAMP(0) WITH TIME ZONE NOT NULL, 
+          PRIMARY KEY(id)
+        )');
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'postgresql',
+            'Migration can only be executed safely on \'postgresql\'.'
+        );
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE blog_post');
+    }
+}
