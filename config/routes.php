1e04abf346c08cefac593b6908f29ebbb6eb1d19
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2017-11-18 17:56:23 +0100

    Moved uuid regex to validator class

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,8 @@
 <?php
 /** @var Zend\Expressive\Application $app */
 
-$uuid4 = '[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}';
+use App\Validator\Uuid;
 
 $app->get('/ping', App\Action\PingAction::class, 'ping');
 $app->get('/blog/post', Blog\Action\PostListAction::class, 'blog.post.list');
-$app->get('/blog/post/{id:'. $uuid4 .'}', Blog\Action\PostAction::class, 'blog.post');
+$app->get('/blog/post/{id:' . Uuid::UUID4_REGEX . '}', Blog\Action\PostAction::class, 'blog.post');
---
+++ src/App/src/Validator/Uuid.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Validator;
+
+final class Uuid
+{
+    const UUID4_REGEX = '[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}';
+}
